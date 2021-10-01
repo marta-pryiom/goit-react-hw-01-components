7f7d066e5ba2854e1ec5f030b96cf2d7455e9b27
marta <Vynnytska1989@gmail.com>
2021-10-01 09:53:53 +0300

    задання 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import user from './user.json';
+import React from 'react';
 import './App.css';
-import {Gallery} from './components/Gallery/Gallery'
+import Profile from './components/Profile/Profile';
+// import ColorPicker from './components/ColorPicker/ColorPicker'
+// import Alert from './components/Alert/Alert';
+// import Container from "./components/Container/Container"
 
+// const colorPickerOptions = [
+//   { label:'red', color: '#F44336'},
+//   { label:'green', color: '#4CAF50'},
+//   { label:'blue', color: '#2196F3'},
+//   { label:'grey', color: '#607D8B'},
+//   { label:'pink', color: '#E91E63'},
+//   { label:'indigo', color: '#3F51B5'}
+// ]
 
 function App() {
   return (
-    <div className="App">
-      <Gallery/>
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Container> */}
+      {/* /* <ColorPicker options = {colorPickerOptions}/> */}
+      {/* <Alert text="В тебе все вийде!!!!" type='error'/>
+      <Alert text="В тебе все вийде!!!!" type='success'/>
+      <Alert text="В тебе все вийде!!!!" type='warning'/> 
+      </Container> */}
     </div>
   );
 }
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,40 @@
+// import  '../user.json';
+import s from './Profile.module.css';
+// console.log(user)
+console.log(s);
+
+function Profile({
+  name,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt={name} className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
